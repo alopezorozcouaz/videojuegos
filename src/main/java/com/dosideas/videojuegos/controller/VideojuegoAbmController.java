15dbdc4e17113a26aae1946af42a3fff89536909
alopezorozcouaz <147572356+alopezorozcouaz@users.noreply.github.com>
2024-04-29 16:41:16 -0500

    Se agregaron y modificaron clases para proporcionar la funcionalidad del jcombo para seleccionar un distribuidor en la página de formvideojuego

--- src/main/java/com/dosideas/videojuegos/controller/VideojuegoAbmController.java
+++ src/main/java/com/dosideas/videojuegos/controller/VideojuegoAbmController.java
@@ -1,6 +1,8 @@
 package com.dosideas.videojuegos.controller;
 
+import com.dosideas.videojuegos.service.DistribuidorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class VideojuegoAbmController {
+    private final DistribuidorService distribuidorService;
+
+    public VideojuegoAbmController(DistribuidorService distribuidorService) {
+        this.distribuidorService = distribuidorService;
+    }
     
     @RequestMapping("/videojuegos/crear")
-    public String mostrarFormAlta(){
+    public String mostrarFormAlta(Model model){
+        model.addAttribute("distribuidores", distribuidorService.buscarTodos());
         return "formvideojuego.html";
     }
 }
---
+++ src/main/java/com/dosideas/videojuegos/service/DistribuidorService.java
@@ -0,0 +1,23 @@
+package com.dosideas.videojuegos.service;
+
+import com.dosideas.videojuegos.domain.Distribuidor;
+import com.dosideas.videojuegos.repository.DistribuidorRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author alopezorozco
+ */
+@Service
+public class DistribuidorService {
+    private final DistribuidorRepository distribuidorRepository;
+
+    public DistribuidorService(DistribuidorRepository distribuidorRepository) {
+        this.distribuidorRepository = distribuidorRepository;
+    }
+    
+    public List<Distribuidor> buscarTodos(){
+        return distribuidorRepository.findAll();
+    }
+}
